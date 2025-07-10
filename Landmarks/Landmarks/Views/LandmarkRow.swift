6774ea986e696cf137ebf96de7e7282154552b1a
Jason <acocoanuts@gmail.com>
2025-07-10 19:30:09 +0800

    Create the row view

---
+++ Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,25 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Jason on 10/7/2025.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LandmarkRow(landmark: landmarks[0])
+}
