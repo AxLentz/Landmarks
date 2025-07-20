0c62f588a0ce1a1e3caebe1b911d6f8c56f596ce
Jason <acocoanuts@gmail.com>
2025-07-20 21:37:24 +0800

    finish - Animating views and transitions

---
+++ Landmarks/Landmarks/Model/Hike.swift
@@ -0,0 +1,30 @@
+//
+//  Hike.swift
+//  Landmarks
+//
+//  Created by Jason on 20/7/2025.
+//
+
+import Foundation
+
+struct Hike: Codable, Hashable, Identifiable {
+    var name: String
+    var id: Int
+    var distance: Double
+    var difficulty: Int
+    var observations: [Observation]
+
+    static var formatter = LengthFormatter()
+
+    var distanceText: String {
+        Hike.formatter.string(fromValue: distance, unit: .kilometer)
+    }
+
+    struct Observation: Codable, Hashable {
+        var distanceFromStart: Double
+        var elevation: Range<Double>
+        var pace: Range<Double>
+        var heartRate: Range<Double>
+    }
+}
+
